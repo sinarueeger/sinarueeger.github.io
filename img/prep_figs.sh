76eb82cad626200d4b0547e0abddf42a9e78d5ea
Sina Rueeger <sina.rueeger@gmail.com>
2018-01-11 11:50:54 +0100

    update

---
+++ img/prep_figs.sh
@@ -0,0 +1,6 @@
+
+convert -quality 100 -resize "131x131" -density 300 ssimp-application.jpg ssimp-application.jpg
+
+convert -quality 100 -resize "131x131" -density 300 ssimp-method.jpg ssimp-method.jpg
+
+convert -quality 100 -resize "131x131" -density 300 ssimp.jpg ssimp.jpg
