340f0a05527f4bebebe12affc00d524d0950978c
Sina Rueeger <sina.rueeger@gmail.com>
2018-04-13 20:59:56 +0200

    academic > ghostwriter

--- img/prep_figs.sh
+++
@@ -1,6 +0,0 @@
-
-convert -quality 100 -resize "400x1300" -density 300 ssimp-application-orig.jpg ssimp-application.jpg
-
-convert -quality 100 -resize "400x1300" -density 300 ssimp-method-orig.jpg ssimp-method.jpg
-
-convert -quality 100 -resize "400x1300" -density 300 ssimp-orig.jpg ssimp.jpg
