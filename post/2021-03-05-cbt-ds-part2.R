d67eba404998c291f792f15a2dd104abd6530f80
Sina Rueger <sina.rueeger@gmail.com>
2021-03-28 18:02:02 +0200

    udpate cbt draft

---
+++ post/2021-03-05-cbt-ds-part2.R
@@ -0,0 +1,30 @@
+---
+title: CBT inspired Data Analysis - part 2
+author: sina rüeger
+date: '2021-03-28'
+slug: cbt-data-analysis-part2
+categories: []
+tags:
+  - quality checks
+  - R
+  - wellbeing
+image:
+  caption: ''
+focal_point: ''
+draft: true
+---
+
+1. Work organisation
+Write code such that mistakes can be corrected and analysis rerun easily. 
+- version control
+- best practices in scientific computing
+
+2. Take a break often
+- ZINE -> debugging
+
+
+3. Good data QC, so high confidence in data
+- Automatic QC tooling
+
+
+4. Familiarize yourself with methods
